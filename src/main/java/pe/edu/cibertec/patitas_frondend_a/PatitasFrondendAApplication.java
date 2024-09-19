f5d525a9e39617467b307ac6cc39105e27125bb4
jere7612 <70173572+jere7612@users.noreply.github.com>
2024-09-18 23:31:17 -0500

    Carga inicial de patitas que dios me dio

---
+++ src/main/java/pe/edu/cibertec/patitas_frondend_a/PatitasFrondendAApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitas_frondend_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitasFrondendAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitasFrondendAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/patitas_frondend_a/PatitasFrondendAApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitas_frondend_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitasFrondendAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
